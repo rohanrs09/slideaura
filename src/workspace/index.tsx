da48efb5fa29bc0b3167a967b4dae815bfe362cd
rohan shelke <rohan_shelke_it@moderncoe.edu.in>
2025-11-06 23:11:09 +0530

    change error

--- src/workspace/index.tsx
+++ src/workspace/index.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
 import { Link, Outlet, useLocation } from "react-router-dom";
-import { firebaseDb } from "../../config/FireBaseConfig";
+import { firebaseDb } from "../../config/FirebaseConfig";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { useEffect } from "react";
 import { useContext } from "react";
